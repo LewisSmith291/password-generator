15e1ce3b491e662de654952e5c4c85f4df53b38a
Lewis Smith <66628534+PKThunda291@users.noreply.github.com>
2026-01-19 12:48:04 +0000

    changed base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/password-generator/',
+  base: 'https://lewissmith291.github.io/password-generator/',
   plugins: [
     react(),
     tailwindcss(),
